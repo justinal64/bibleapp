28649b1088a5c2442de65b95a81eb01af2b90c22
justin leggett <justin@codingrookie.com>
2018-07-17 21:53:47 -0500

    added default styling

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,42 +1,44 @@
 import * as React from "react";
-// import Header from "../components/typography/header";
 import { Row, Col } from "react-flexbox-grid";
-import Link from "../components/typography/link";
-// import StyledButton from "../components/typography/button";
 import styled from "styled-components";
 import { PrimaryButton } from "../components/utils/styling";
-const CustomLink = styled.a`
+
+const StyledLink = styled.a`
   ${PrimaryButton};
+  text-decoration: none;
   line-height: 2em;
   height: 100%;
   width: 100%;
   display: inline-block;
 `;
 
-const CustomDiv = styled.div`
+const StyledDiv = styled.div`
   height: 2em;
+`;
+
+const StyledHr = styled.hr`
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        {/* <Row center="lg">
-          <Header title="This is a title" font={1.25} />
-        </Row> */}
         <Row center="sm" middle="lg">
-          <Col lgOffset={1} lg={4}>
-            {/* <Link href="/signup" title="Sponsor" />
-            <StyledButton title="Sponsor" font={3} /> */}
-            <CustomDiv>
-              <CustomLink href="/bible">Sponsor</CustomLink>
-            </CustomDiv>
+          <Col lg={4}>
+            <StyledDiv>
+              <StyledLink href="/sponsor">Sponsor</StyledLink>
+            </StyledDiv>
           </Col>
           <Col lg={2}>
-            <hr />
+            <StyledHr />
           </Col>
-          <Col lg={5}>
-            <Link href="/signup" title="Participant" />
+          <Col lg={4}>
+            <StyledDiv>
+              <StyledLink href="/participate">Participate</StyledLink>
+            </StyledDiv>
           </Col>
         </Row>
       </div>
